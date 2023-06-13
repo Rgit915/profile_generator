846fa72928ae4f878b601f109c9bba370b0dd924
Rora Alem <rora.goitom@gmail.com>
2023-06-13 17:13:59 -0400

    add multiple survey questions

--- survey.js
+++ survey.js
@@ -1,12 +1,24 @@
+//import the 'readline' module
 const readline = require('readline');
-
+// set up the input and output streams for CLI
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
-
-rl.question('What do you think of Node.js? ', (answer) => {
-  console.log(`Thank you for your valuable feedback: ${answer}`);
-
-  rl.close();
+//question method to prompt the user with the following questions
+rl.question("What's your name? Nicknames are also acceptable :) ", (name) => { //callback function executed when user provides an answer
+  rl.question("What's an activity you like doing? ", (activity) => {
+    rl.question("What do you listen to while doing that? ", (music) => {
+      rl.question("Which meal is your favourite(e.g: dinner, lunch, etc.) ", (meal) => {
+        rl.question(`What's your favourite thing to eat for that meal? `, (food) =>{
+          rl.question("Which sport is your absolute favourite? ", (sport) => {
+            rl.question("What's your superpower? In a few words, tell us what you are amazing at! ", (superPower) =>{
+              console.log(`${name} enjoys ${activity}, listens to ${music}, loves ${food} for ${meal}, is a fan of ${sport}, and possesses the incredible superpower of ${superPower}.`);
+              rl.close();   //close CLI and program exit
+            });
+          });
+        });
+      });
+    });
+  });
 });

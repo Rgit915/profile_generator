68dac2c1374eff4c4118c8a9a994560658b04a6f
Rora Alem <rora.goitom@gmail.com>
2023-06-13 18:23:14 -0400

    change question to use template literals

--- survey.js
+++ survey.js
@@ -8,9 +8,9 @@ const rl = readline.createInterface({
 //question method to prompt the user with the following questions
 rl.question("What's your name? Nicknames are also acceptable :) ", (name) => { //callback function executed when user provides an answer
   rl.question("What's an activity you like doing? ", (activity) => {
-    rl.question("What do you listen to while doing that? ", (music) => {
+    rl.question(`What do you listen to while ${activity}? `, (music) => {
       rl.question("Which meal is your favourite(e.g: dinner, lunch, etc.) ", (meal) => {
-        rl.question(`What's your favourite thing to eat for that meal? `, (food) =>{
+        rl.question(`What's your favourite thing to eat for ${meal}? `, (food) =>{
           rl.question("Which sport is your absolute favourite? ", (sport) => {
             rl.question("What's your superpower? In a few words, tell us what you are amazing at! ", (superPower) =>{
               console.log(`${name} enjoys ${activity}, listens to ${music}, loves ${food} for ${meal}, is a fan of ${sport}, and possesses the incredible superpower of ${superPower}.`);
